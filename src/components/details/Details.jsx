8a97822c387aaf12dce232995d7115d03aca7d39
LeviathanS11 <shakiulalamsas11@gmail.com>
2023-05-05 19:09:50 +0600

    react tostify added

--- src/components/details/Details.jsx
+++ src/components/details/Details.jsx
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Card } from 'react-bootstrap';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-const Details = ({detail}) => {
-    const{name,ingredients, cooking_method,rating,image}=detail;
+const Details = ({ detail }) => {
+    const handleClick = event => {
+        event.currentTarget.disabled = true;
+        toast('added to favorite')
+    };
+    const { name, ingredients, cooking_method, rating, image } = detail;
     return (
-        <Card  style={{ width: '20rem' }}>
-                <Card.Body>
+        <Card style={{ width: '20rem' }}>
+            <Card.Body>
                 <Card.Img variant="top" src={image} />
-                    <Card.Title>{name}</Card.Title>
-                    <Card.Text><b>Ingredients:</b> {ingredients}</Card.Text>
-                    <Card.Text><b>Method:</b>{cooking_method}</Card.Text>
-                    <Card.Text><b>Rating:</b>{rating}</Card.Text>
-                    <button className='hover'> Favorite</button>
-                </Card.Body>
-            </Card>
+                <Card.Title>{name}</Card.Title>
+                <Card.Text><b>Ingredients:</b> {ingredients}</Card.Text>
+                <Card.Text><b>Method:</b>{cooking_method}</Card.Text>
+                <Card.Text><b>Rating:</b>{rating}</Card.Text>
+                <div>
+                    <button onClick={handleClick} className='hover'> Favorite</button>
+                    <ToastContainer/>
+                </div>
+            </Card.Body>
+        </Card>
     );
 };
 
